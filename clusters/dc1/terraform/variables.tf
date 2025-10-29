94abc3de134c1414f2702b21fe40f6b65f198dd2
Muhammad Arif Wicaksana <194448420+marifwicaksana@users.noreply.github.com>
2025-10-29 15:24:47 +0700

    fixed cluster name

--- clusters/dc1/terraform/variables.tf
+++ clusters/dc1/terraform/variables.tf
@@ -44,6 +44,7 @@ variable "numclients" {
 }
 variable "cluster_name" {
   description = "Name of the cluster"
+  default     = "cluster-dc1"
 }
 variable "owner" {
   description = "Owner of the cluster"
--- clusters/dc2/terraform/variables.tf
+++ clusters/dc2/terraform/variables.tf
@@ -44,6 +44,7 @@ variable "numclients" {
 }
 variable "cluster_name" {
   description = "Name of the cluster"
+  default     = "cluster-dc2"
 }
 variable "owner" {
   description = "Owner of the cluster"
